3236d2b7b065f582084b20083fe8db791d8d1ea6
K.Morita <jhcelium@gmail.com>
2026-06-15 00:08:59 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://supplementsingapore.neoi.jp",
   output: "static",
-  integrations: [react(), sitemap()],
+  integrations: [react()],
 });
